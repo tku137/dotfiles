4224391affc99a02ad985457b9f34c6d2d9118b7
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-16 21:18:43 +0200

    fish: remove conda
    this drastically slows down terminal startup :(

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,10 +6,3 @@ direnv hook fish | source
 fzf_configure_bindings --directory=\cf
 zoxide init fish | source
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /usr/local/anaconda3/bin/conda
-    eval /usr/local/anaconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-
