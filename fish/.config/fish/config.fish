1121911708befdb6ca2d655fafb0e4810a715ea8
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2023-07-31 19:32:24 +0200

    force english in fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+set -gx LANG "en_US.UTF-8"
 starship init fish | source
 direnv hook fish | source
 fzf_configure_bindings --directory=\cf
