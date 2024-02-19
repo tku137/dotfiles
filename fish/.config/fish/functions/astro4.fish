7615bc276267fc256c0820eec1bb539e464686d2
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-19 22:11:26 +0100

    fish: add alias for astronvim v4 nvim_appname

---
+++ fish/.config/fish/functions/astro4.fish
@@ -0,0 +1,4 @@
+function astro4 --wraps='env NVIM_APPNAME=astronvim_v4 nvim' --description 'alias astro4=env NVIM_APPNAME=astronvim_v4 nvim'
+  env NVIM_APPNAME=astronvim_v4 nvim $argv
+        
+end
