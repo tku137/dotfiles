606ea18cf99c80c43e7a818c5abf9aa65b3fe912
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2024-02-22 09:35:03 +0100

    fish: add shorter astronvim_v4 alias

--- fish/.config/fish/functions/astro4.fish
+++ fish/.config/fish/functions/astro4.fish
@@ -1,4 +1,7 @@
-function astro4 --wraps='env NVIM_APPNAME=astronvim_v4 nvim' --description 'alias astro4=env NVIM_APPNAME=astronvim_v4 nvim'
+function astro4 --wraps='env NVIM_APPNAME=astronvim_v4 nvim' --description 'Run AstroNVim v4 config'
   env NVIM_APPNAME=astronvim_v4 nvim $argv
-        
+end
+
+function an4 --wraps='env NVIM_APPNAME=astronvim_v4 nvim' --description 'Run AsroNVim v4 config'
+  env NVIM_APPNAME=astronvim_v4 nvim $argv
 end
