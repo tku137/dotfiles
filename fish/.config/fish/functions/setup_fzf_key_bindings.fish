4c96f4a45159475a8fd455a46a6e500aea3d7b9d
Tony Fischer <tony@getml.com>
2024-03-11 13:39:37 +0100

    fish: dynamically set up fzf_key_bindings.fish

---
+++ fish/.config/fish/functions/setup_fzf_key_bindings.fish
@@ -0,0 +1,23 @@
+function fzf_key_bindings --description 'Set the correct path for fzf key bindings based on architecture'
+    set -l arch (uname -m)
+
+    switch $arch
+        case 'x86_64'
+            set fzf_path /usr/local/opt/fzf/shell/key-bindings.fish
+        case 'arm64'
+            set fzf_path /opt/homebrew/fzf/shell/key-bindings.fish
+        case '*'
+            echo "Unsupported architecture: $arch"
+            return 1
+    end
+
+    if test -f $fzf_path
+        source $fzf_path
+    else
+        echo "fzf key bindings file not found at $fzf_path"
+    end
+end
+
+# Call the function to ensure the key bindings are set
+fzf_key_bindings
+
