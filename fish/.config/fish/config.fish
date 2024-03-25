543eba92fe19374250ed9bf88ac2e17807c97274
Tony Fischer <tony@getml.com>
2024-03-25 11:38:13 +0100

    hatch: add global config for hatch

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -42,3 +42,11 @@ end
 
 # fix libgit2 dyld path on intel macs
 set -x DYLD_LIBRARY_PATH /usr/local/opt/libgit2/lib $DYLD_LIBRARY_PATH
+
+# set XDG home directories
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+
+# set hatch config path
+set -gx HATCH_CONFIG "$XDG_CONFIG_HOME/hatch/config.toml"
