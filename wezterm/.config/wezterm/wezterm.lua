498d5ca8398007b0d7fecf4af514bfbfa71d9a74
Tony Fischer (tku137) <tonyfischer@mailbox.org>
2025-03-07 08:44:25 +0100

    feat(wezterm): cleanup

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,9 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
 
--- Pull in the smart_splits.nvim plugin
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
-
 -- Create base config object
 local config = wezterm.config_builder and wezterm.config_builder() or {}
 
@@ -230,29 +227,12 @@ wezterm.on("user-var-changed", function(window, pane, name, value)
 	window:set_config_overrides(overrides)
 end)
 
--- Apply smart_splits.nvim configuration
--- smart_splits.apply_to_config(config, {
--- 	-- the default config is here, if you'd like to use the default keys,
--- 	-- you can omit this configuration table parameter and just use
--- 	-- smart_splits.apply_to_config(config)
---
--- 	-- directional keys to use in order of: left, down, up, right
--- 	-- direction_keys = { "h", "j", "k", "l" },
--- 	-- if you want to use separate direction keys for move vs. resize, you
--- 	-- can also do this:
--- 	direction_keys = {
--- 		move = { "h", "j", "k", "l" },
--- 		resize = { "LeftArrow", "DownArrow", "UpArrow", "RightArrow" },
--- 	},
--- 	-- modifier keys to combine with direction_keys
--- 	modifiers = {
--- 		move = "META", -- modifier to use for pane movement, e.g. CTRL+h to move left
--- 		resize = "META", -- modifier to use for pane resize, e.g. META+h to resize to the left
--- 	},
--- })
-
 -- Disable the terminal bell
 config.audible_bell = "Disabled"
+
+-- Terminal tweaks
+config.term = "xterm-kitty"
+config.enable_kitty_graphics = true
 
 -- Return the configuration
 return config
