1e4484db75f57e4a6f0252fee45c8a3ec6bd2805
Tony Fischer (tku137) <tku137@mailbox.org>
2025-04-06 18:22:20 +0200

    feat(nvim): migrate to lualine
    heirline.nvim is very complex to configure and atm does not provide any
    benefit over lualine. Also, since heirline is rather a statusline coding
    kit and one needs to take care of a lot of things, it introduces way too
    much friction. For example, I needed to implement a hack to get instant
    mode updates, which is not what I want nor need.

---
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,42 @@
+local colors = require("tokyonight.colors").setup()
+
+local project_root = {
+  function()
+    return vim.fn.fnamemodify(vim.fn.getcwd(), ":t")
+  end,
+  icon = "󱉭",
+  separator = "",
+  color = { fg = colors.blue },
+}
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "bwpge/lualine-pretty-path",
+    },
+    opts = {
+      options = {
+        theme = "auto",
+        globalstatus = vim.o.laststatus == 3,
+        disabled_filetypes = { statusline = { "snacks_dashboard" } },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { project_root, "pretty_path" },
+        lualine_x = { "filetype", "lsp_status" },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          function()
+            return " " .. os.date("%R")
+          end,
+        },
+      },
+    },
+  },
+}
