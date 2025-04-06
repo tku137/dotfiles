a9f438442333247342f94071e8aaefcd334a166b
Tony Fischer (tku137) <tku137@mailbox.org>
2025-04-06 21:57:55 +0200

    feat(nvim): add snacks utils and session management

--- nvim/.config/nvim/lua/plugins/ui/components.lua
+++ nvim/.config/nvim/lua/plugins/ui/components.lua
@@ -69,6 +69,7 @@ return {
           keys = {
             { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
             { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+            { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
             { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             {
