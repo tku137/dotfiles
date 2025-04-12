44649b083869eb14a909c2e12d9f824d71c622d8
Tony Fischer (tku137) <tku137@mailbox.org>
2025-04-12 23:52:47 +0200

    feat(nvim): add noice; add lualine components; improve snacks loading ;cleanup

--- nvim/.config/nvim/lua/plugins/ui/lualine.lua
+++ nvim/.config/nvim/lua/plugins/ui/lualine.lua
@@ -1,5 +1,7 @@
 local colors = require("tokyonight.colors").setup()
 
+local Snacks = require("snacks")
+
 local project_root = {
   require("utils.helpers").project_root,
   icon = "󱉭",
@@ -8,32 +10,55 @@ local project_root = {
 }
 
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-      "bwpge/lualine-pretty-path",
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+    "bwpge/lualine-pretty-path",
+    "folke/snacks.nvim", -- ensure snacks loads first
+  },
+  opts = {
+    options = {
+      theme = "auto",
+      globalstatus = vim.o.laststatus == 3,
+      disabled_filetypes = { statusline = { "snacks_dashboard" } },
     },
-    opts = {
-      options = {
-        theme = "auto",
-        globalstatus = vim.o.laststatus == 3,
-        disabled_filetypes = { statusline = { "snacks_dashboard" } },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { project_root, "pretty_path" },
+      -- lualine_x = { "filetype", "lsp_status" },
+      lualine_x = {
+        Snacks.profiler.status(),
+        -- Displays showcmd
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.command.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+          color = function() return { fg = Snacks.util.color("Statement") } end,
+        },
+        -- Displays showmode, for example macro recording
+        -- stylua: ignore
+        {
+          function() return require("noice").api.status.mode.get() end,
+          cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+          color = function() return { fg = Snacks.util.color("Constant") } end,
+        },
+        -- stylua: ignore
+        -- {
+        --   function() return "  " .. require("dap").status() end,
+        --   cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+        --   color = function() return { fg = Snacks.util.color("Debug") } end,
+        -- },
       },
-      sections = {
-        lualine_a = { "mode" },
-        lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = { project_root, "pretty_path" },
-        lualine_x = { "filetype", "lsp_status" },
-        lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
-        },
-        lualine_z = {
-          function()
-            return " " .. os.date("%R")
-          end,
-        },
+      lualine_y = {
+        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+        { "location", padding = { left = 0, right = 1 } },
+      },
+      lualine_z = {
+        function()
+          return " " .. os.date("%R")
+        end,
       },
     },
   },
