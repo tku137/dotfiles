3920d900bc65a0b7f1d8485d0e7f65daf1ed588f
Tony Fischer (tku137) <tku137@mailbox.org>
2025-04-20 20:28:31 +0200

    feat(nvim): add treesitter

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -24,6 +24,7 @@ require("lazy").setup({
     -- import plugins
     { import = "plugins" },
     { import = "plugins.ui" },
+    { import = "plugins.core" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -0,0 +1,130 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      local treesitter = require("nvim-treesitter.configs")
+
+      ---@type TSConfig
+      ---@diagnostic disable-next-line: missing-fields
+      treesitter.setup({
+
+        -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+        ensure_installed = {
+          "bash",
+          "c",
+          "diff",
+          "json",
+          "jsonc",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "printf",
+          "query",
+          "regex",
+          "toml",
+          "vim",
+          "vimdoc",
+          "yaml",
+        },
+
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
+
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
+
+        -- Modules
+        -- Consistent syntax highlighting.
+        highlight = { enable = true },
+
+        -- Indentation based on treesitter for the = operator.
+        indent = { enable = true },
+
+        -- Incremental selection based on the named nodes from the grammar.
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<C-space>",
+            node_incremental = "<C-space>",
+            scope_incremental = false,
+            node_decremental = "<BS>",
+          },
+        },
+
+        -- Add all the textobjects modules in one place:
+        textobjects = {
+          -- Selection: af/if around functions, ac/ic around classes, ap/ip for params
+          select = {
+            enable = true,
+            lookahead = true, -- jump forward to textobject
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["ap"] = "@parameter.outer",
+              ["ip"] = "@parameter.inner",
+            },
+          },
+
+          -- Movement: ]f/[f to go to next/prev function, ]c/[c to class, etc.
+          move = {
+            enable = true,
+            set_jumps = true, -- add to jumplist
+            goto_next_start = {
+              ["]f"] = "@function.outer",
+              ["]c"] = "@class.outer",
+              ["]a"] = "@parameter.inner",
+            },
+            goto_next_end = {
+              ["]F"] = "@function.outer",
+              ["]C"] = "@class.outer",
+              ["]A"] = "@parameter.inner",
+            },
+            goto_previous_start = {
+              ["[f"] = "@function.outer",
+              ["[c"] = "@class.outer",
+              ["[a"] = "@parameter.inner",
+            },
+            goto_previous_end = {
+              ["[F"] = "@function.outer",
+              ["[C"] = "@class.outer",
+              ["[A"] = "@parameter.inner",
+            },
+          },
+
+          -- Swap: <leader>a swaps next parameter, <leader>A previous
+          -- TODO: find better keymaps
+          -- swap = {
+          --   enable = true,
+          --   swap_next = {
+          --     ["<leader>a"] = "@parameter.inner",
+          --   },
+          --   swap_previous = {
+          --     ["<leader>A"] = "@parameter.inner",
+          --   },
+          -- },
+
+          -- LSP Interop: peek definition of function/class under cursor
+          -- TODO: check if this is useful
+          -- lsp_interop = {
+          --   enable = true,
+          --   border = "single",
+          --   peek_definition_code = {
+          --     ["<leader>df"] = "@function.outer",
+          --     ["<leader>dF"] = "@class.outer",
+          --   },
+          -- },
+        },
+      })
+    end,
+  },
+}
