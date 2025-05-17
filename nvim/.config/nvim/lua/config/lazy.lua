1a4ba4879816e59646957048cb4a43e7e7511867
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-17 19:59:13 +0200

    refactor(nvim): move which-key to editor plugins

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -23,8 +23,9 @@ require("lazy").setup({
   spec = {
     -- import plugins
     { import = "plugins" },
-    { import = "plugins.ui" },
     { import = "plugins.core" },
+    { import = "plugins.editor" },
+    { import = "plugins.ui" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
