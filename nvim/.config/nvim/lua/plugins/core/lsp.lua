f1ff9816e774ac4a79a91e35a1fcaac15dea4191
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-19 09:59:58 +0200

    feat(nvim): add LSP basic LSP config; add lua_ls
    This enables LSP functionality using the new LSP API of nvim 0.11+ and
    adds lua_ls as the fundamental LSP.

---
+++ nvim/.config/nvim/lua/plugins/core/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.lsp.enable({
+        "lua_ls",
+      })
+    end,
+  },
+}
