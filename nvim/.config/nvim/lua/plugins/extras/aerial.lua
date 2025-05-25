41c30f3a9555c8f56efac37b030c69de9916e239
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-25 21:20:30 +0200

    feat(nvim): add aerial

---
+++ nvim/.config/nvim/lua/plugins/extras/aerial.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "stevearc/aerial.nvim",
+    event = { "BufReadPost", "BufNewFile", "BufWritePost" },
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      attach_mode = "global",
+      backends = { "lsp", "treesitter", "markdown", "man" },
+      layout = {
+        default_direction = "prefer_right", -- open at right; left if not enough room
+        min_width = 28,
+        resize_to_content = false,
+      },
+      show_guides = true, -- thin ASCII tree lines
+      highlight_on_jump = 300, -- flash symbol under cursor (ms)
+      icons = require("config.icons").kinds,
+    },
+    keys = {
+      { "<leader>cs", "<cmd>AerialToggle!<cr>", desc = "Symbols (Aerial)" },
+    },
+  },
+}
