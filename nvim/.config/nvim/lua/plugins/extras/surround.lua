3dcefdab6a1b5de0b49b1093ebff7ad11c70cc1e
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-25 19:34:20 +0200

    feat(nvim): add mini.surround

---
+++ nvim/.config/nvim/lua/plugins/extras/surround.lua
@@ -0,0 +1,18 @@
+-- Fast and feature-rich surround actions. For text that includes
+-- surrounding characters like brackets or quotes, this allows you
+-- to select the text inside, change or modify the surrounding characters,
+-- and more.
+return {
+  "echasnovski/mini.surround",
+  opts = {
+    mappings = {
+      add = "gsa", -- Add surrounding in Normal and Visual modes
+      delete = "gsd", -- Delete surrounding
+      find = "gsf", -- Find surrounding (to the right)
+      find_left = "gsF", -- Find surrounding (to the left)
+      highlight = "gsh", -- Highlight surrounding
+      replace = "gsr", -- Replace surrounding
+      update_n_lines = "gsn", -- Update `n_lines`
+    },
+  },
+}
