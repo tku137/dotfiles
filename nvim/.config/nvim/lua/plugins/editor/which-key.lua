76d8cf41acc183708abe352fec70fd3ed83f79ef
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-26 14:09:57 +0200

    feat(nvim): add neotest with proper adapter config mechanism

--- nvim/.config/nvim/lua/plugins/editor/which-key.lua
+++ nvim/.config/nvim/lua/plugins/editor/which-key.lua
@@ -21,6 +21,7 @@ return {
           { "<leader>q", group = "quit/session" },
           { "<leader>s", group = "search" },
           { "<leader>sr", group = "replace", icon = { icon = "󰛔 ", color = "blue" } },
+          { "<leader>t", group = "test" },
           { "<leader>u", group = "ui", icon = { icon = "󰙵 ", color = "cyan" } },
           { "<leader>x", group = "diagnostics/quickfix", icon = { icon = "󱖫 ", color = "green" } },
           { "[", group = "prev" },
