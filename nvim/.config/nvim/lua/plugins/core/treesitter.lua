073bf7ff900863036976a889c83bfaa6aec68cdf
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-27 11:39:41 +0200

    perf(nvim): further improve treesitter config

--- nvim/.config/nvim/lua/plugins/core/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -1,21 +1,38 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    -- branch = "main",
-    build = ":TSUpdate",
-    -- main = "nvim-treesitter.configs", -- Sets main module to use for opts
-    lazy = true,
-    event = { "BufReadPost", "BufNewFile" },
-    -- event = "BufReadPre",
-    ---@param opts TSConfig
-    config = function(_, opts)
-      -- **Important:** use the configs module to setup
-      require("nvim-treesitter.configs").setup(opts) -- load the above options
-    end,
+    main = "nvim-treesitter.configs",
     dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-      -- { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
+      { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
     },
+    event = "VeryLazy",
+    lazy = vim.fn.argc(-1) == 0, -- load treesitter immediately when opening a file from the cmdline
+    cmd = {
+      "TSBufDisable",
+      "TSBufEnable",
+      "TSBufToggle",
+      "TSDisable",
+      "TSEnable",
+      "TSToggle",
+      "TSInstall",
+      "TSInstallInfo",
+      "TSInstallSync",
+      "TSModuleInfo",
+      "TSUninstall",
+      "TSUpdate",
+      "TSUpdateSync",
+    },
+    build = ":TSUpdate",
+    init = function(plugin)
+      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
+      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
+      -- during startup.
+      -- CODE FROM LazyVim (thanks folke!) https://github.com/LazyVim/LazyVim/commit/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9
+      require("lazy.core.loader").add_to_rtp(plugin)
+      pcall(require, "nvim-treesitter.query_predicates")
+    end,
     opts_extend = { "ensure_installed" },
     opts = {
 
