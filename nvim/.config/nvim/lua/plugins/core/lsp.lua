68beafa3a988e5031e273e3a7671d6f2c6a35f0b
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-28 21:28:02 +0200

    feat(nvim): sane defaults for LSP; toggle diagnostics display

--- nvim/.config/nvim/lua/plugins/core/lsp.lua
+++ nvim/.config/nvim/lua/plugins/core/lsp.lua
@@ -1,6 +1,25 @@
 local icons = require("config.icons").diagnostics
 
+-- Default config to show diagnostics as virtual text
+local diagnostic_virtual_text_config = {
+  virtual_text = { spacing = 4, prefix = "●" },
+  virtual_lines = false,
+}
+
+-- Default config to show diagnostics as virtual lines
+local diagnostic_virtual_lines_config = {
+  virtual_text = false,
+  virtual_lines = { only_current_line = false },
+}
+
+-- Set virtual text for diagnostics as default
+vim.diagnostic.config(diagnostic_virtual_text_config)
+
+-- Configure all other global diagnostics settings
 vim.diagnostic.config({
+  underline = true,
+  update_in_insert = false,
+  severity_sort = true,
   signs = {
     text = {
       [vim.diagnostic.severity.ERROR] = icons.Error,
@@ -8,6 +27,35 @@ vim.diagnostic.config({
       [vim.diagnostic.severity.INFO] = icons.Info,
       [vim.diagnostic.severity.HINT] = icons.Hint,
     },
+  },
+  -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+  -- Be aware that you also will need to properly configure your LSP server to
+  -- provide the inlay hints.
+  inlay_hints = {
+    enabled = true,
+    exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+  },
+  -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+  -- Be aware that you also will need to properly configure your LSP server to
+  -- provide the code lenses.
+  codelens = {
+    enabled = false,
+  },
+  -- add any global capabilities here
+  capabilities = {
+    workspace = {
+      fileOperations = {
+        didRename = true,
+        willRename = true,
+      },
+    },
+  },
+  -- options for vim.lsp.buf.format
+  -- `bufnr` and `filter` is handled by the LazyVim formatter,
+  -- but can be also overridden when specified
+  format = {
+    formatting_options = nil,
+    timeout_ms = nil,
   },
 })
 
@@ -42,6 +90,29 @@ return {
       -- opts.servers is a list of servers to enable, merged from all
       -- language specific config files
       vim.lsp.enable(opts.servers)
+
+      -- Which-Key toggle to swithc between displaying of diagnostics
+      -- as either virtual text or virtual lines
+      Snacks.toggle
+        .new({
+          name = "Virtual Line Diagnostics",
+          get = function()
+            local vlines_current_config = vim.diagnostic.config().virtual_lines
+            -- Virtual lines are considered active if their configuration value
+            -- is not nil AND not false.
+            return vlines_current_config ~= nil and vlines_current_config ~= false
+          end,
+          set = function(inline)
+            if inline then
+              -- Set virtual text settings defined above
+              vim.diagnostic.config(diagnostic_virtual_lines_config)
+            else
+              -- Set virtual lines settings defined above
+              vim.diagnostic.config(diagnostic_virtual_text_config)
+            end
+          end,
+        })
+        :map("<leader>uv")
     end,
   },
 
