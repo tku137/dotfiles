91acea62b5dcf854b1340f41493eddc488a07059
Tony Fischer (tku137) <tku137@mailbox.org>
2025-05-31 19:51:08 +0200

    feat(nvim): add fish config

--- nvim/.config/nvim/lua/plugins/languages/fish.lua
+++ nvim/.config/nvim/lua/plugins/languages/fish.lua
@@ -6,11 +6,25 @@ return {
     opts = { ensure_installed = { "fish" } },
   },
 
+  -- LSP
+  -- brew install fish-lsp
+  {
+    "neovim/nvim-lspconfig",
+    opts = { servers = { "fish_lsp" } },
+  },
+
   -- Formatter
   -- fish_indent comes with the fish shell :)
   {
     "stevearc/conform.nvim",
     opts_extend = { "formatters_by_ft.fish" }, -- important to convince lazy.nvim to merge this!
     opts = { formatters_by_ft = { fish = { "fish_indent" } } },
+  },
+
+  -- Linter
+  {
+    "mfussenegger/nvim-lint",
+    opts_extend = { "linters_by_ft.fish" }, -- important to convince lazy.nvim to merge this!
+    opts = { linters_by_ft = { fish = { "fish" } } },
   },
 }
