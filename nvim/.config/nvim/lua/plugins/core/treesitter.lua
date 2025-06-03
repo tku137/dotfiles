4497048815ddef35a9f5a4d2bc1c37f63a6d9bba
Tony Fischer (tku137) <tku137@mailbox.org>
2025-06-03 09:55:22 +0200

    feat(nvim): have misc treesitter langs in modularized file

--- nvim/.config/nvim/lua/plugins/core/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -41,18 +41,13 @@ return {
     opts = {
 
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = {
-        "bash",
-
-        "rst",
-        "query",
-        "regex",
-        "vim",
-        "vimdoc",
-        "fish",
-        "kdl",
-        "ssh_config",
-      },
+      -- INFO: To add language specific parsers in their own config file, use this snippet:
+      -- {
+      --   "nvim-treesitter/nvim-treesitter",
+      --   opts = { ensure_installed = { "python", "requirements" } },
+      -- },
+      -- INFO: basic and misc parsers are defined in languages/misc.lua
+      ensure_installed = {},
 
       -- Automatically install missing parsers when entering buffer
       -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
