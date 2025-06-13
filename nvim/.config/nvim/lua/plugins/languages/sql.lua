15dfb6f11b54eb08898f893eb17cfdd98c72f173
Tony Fischer (tku137) <tku137@mailbox.org>
2025-06-13 16:15:58 +0200

    feat(sql): add sql config

---
+++ nvim/.config/nvim/lua/plugins/languages/sql.lua
@@ -0,0 +1,58 @@
+-- Add a keymap for toggling postgres_lsp
+-- This toggles postgres_lsp on/off using the new Neovim 0.11+ vim.lsp API
+Snacks.toggle
+  .new({
+    name = "PostgreSQL LSP",
+    get = function()
+      local clients = vim.lsp.get_clients({ name = "postgres_lsp" })
+      return #clients > 0
+    end,
+    set = function()
+      require("utils.lsp_utils").toggle_postgres_lsp({ silent = true })
+    end,
+  })
+  :map("<leader>cP")
+
+return {
+
+  -- Treesitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "sql" } },
+  },
+
+  -- LSP
+  -- (no brew bottle available)
+  -- mise use -g npm:@postgrestools/postgrestools@latest
+  {
+    "neovim/nvim-lspconfig",
+    opts = { servers = { "sqlls", "postgres_lsp" } },
+  },
+
+  -- Formatter
+  -- brew install sqlfluff
+  -- OR
+  -- mise use -g npm:sql-language-server@latest
+  -- mise use -g pipx:sqlfluff@latest
+  {
+    "stevearc/conform.nvim",
+    opts_extend = { "formatters_by_ft.sql" }, -- important to convince lazy.nvim to merge this!
+    opts = { formatters_by_ft = { sql = { "sqlfluff" } } },
+  },
+
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup(--[[optional config]])
+    end,
+  },
+}
