a155c6978205b1b47396e76cd23cddf8a05c72d4
Tony Fischer (tku137) <tku137@mailbox.org>
2025-08-07 21:50:36 +0200

    fix(fish): recreation of fish functions on some occasions

--- fish/.config/fish/conf.d/01-init.fish
+++ fish/.config/fish/conf.d/01-init.fish
@@ -1,10 +1,10 @@
 # fix missing fzf key bindings
 source $HOME/.config/fish/functions/fzf_configure_bindings.fish
 
-# fix missing functions
-for file in $HOME/.config/fish/functions/*.fish
-    source $file
-end
+# fix missing functions - DISABLED to prevent funcsave spam from fish-eza plugin
+# for file in $HOME/.config/fish/functions/*.fish
+#     source $file
+# end
 
 # fix missing user functions
 set -gx fish_function_path $HOME/.config/fish/functions/my_functions $fish_function_path
