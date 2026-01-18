6257cb7f5b34912b3f69bd69db63d3c350f6103a
Tony Fischer (tku137) <tku137@mailbox.org>
2026-01-18 17:24:21 +0100

    first attempt

--- nvim/.config/nvim/lua/plugins/core/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -1,41 +1,23 @@
+local function uniq(list)
+  local seen, out = {}, {}
+  for _, v in ipairs(list or {}) do
+    if type(v) == "string" and not seen[v] then
+      seen[v] = true
+      table.insert(out, v)
+    end
+  end
+  return out
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    main = "nvim-treesitter.configs",
-    dependencies = {
-      { "nvim-treesitter/nvim-treesitter-textobjects", lazy = true },
-    },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter immediately when opening a file from the cmdline
-    event = { "BufReadPre", "BufNewFile" },
-    cmd = {
-      "TSBufDisable",
-      "TSBufEnable",
-      "TSBufToggle",
-      "TSDisable",
-      "TSEnable",
-      "TSToggle",
-      "TSInstall",
-      "TSInstallInfo",
-      "TSInstallSync",
-      "TSModuleInfo",
-      "TSUninstall",
-      "TSUpdate",
-      "TSUpdateSync",
-    },
+    branch = "main",
+    lazy = false,
     build = function()
       -- silently update all treesitter parsers on fresh install
       -- otherwise installation can "hang" when a bunch of parsers are installed
       vim.cmd("silent! TSUpdate")
-    end,
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treeitter** module to be loaded in time.
-      -- Luckily, the only thins that those plugins need are the custom queries, which we make available
-      -- during startup.
-      -- CODE FROM LazyVim (thanks folke!) https://github.com/LazyVim/LazyVim/commit/1e1b68d633d4bd4faa912ba5f49ab6b8601dc0c9
-      require("lazy.core.loader").add_to_rtp(plugin)
-      pcall(require, "nvim-treesitter.query_predicates")
     end,
     opts_extend = { "ensure_installed" },
     opts = {
@@ -49,9 +31,8 @@ return {
       -- INFO: basic and misc parsers are defined in languages/misc.lua
       ensure_installed = {},
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+      -- recommended install dir for the rewrite (and added to rtp by setup)
+      install_dir = vim.fn.stdpath("data") .. "/site",
 
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
@@ -62,72 +43,100 @@ return {
 
       -- Indentation based on treesitter for the = operator.
       indent = { enable = true },
-
-      -- Add all the textobjects modules in one place:
-      textobjects = {
-        -- Selection: af/if around functions, ac/ic around classes, ap/ip for params
-        select = {
-          enable = true,
-          lookahead = true, -- jump forward to textobject
-          keymaps = {
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
-            ["ap"] = "@parameter.outer",
-            ["ip"] = "@parameter.inner",
-          },
-        },
-
-        -- Movement: ]f/[f to go to next/prev function, ]c/[c to class, etc.
-        move = {
-          enable = true,
-          set_jumps = true, -- add to jumplist
-          goto_next_start = {
-            ["]f"] = "@function.outer",
-            ["]c"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
-          },
-          goto_next_end = {
-            ["]F"] = "@function.outer",
-            ["]C"] = "@class.outer",
-            ["]A"] = "@parameter.inner",
-          },
-          goto_previous_start = {
-            ["[f"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[a"] = "@parameter.inner",
-          },
-          goto_previous_end = {
-            ["[F"] = "@function.outer",
-            ["[C"] = "@class.outer",
-            ["[A"] = "@parameter.inner",
-          },
-        },
-
-        -- Swap: <leader>a swaps next parameter, <leader>A previous
-        -- TODO: find better keymaps
-        -- swap = {
-        --   enable = true,
-        --   swap_next = {
-        --     ["<leader>a"] = "@parameter.inner",
-        --   },
-        --   swap_previous = {
-        --     ["<leader>A"] = "@parameter.inner",
-        --   },
-        -- },
-
-        -- LSP Interop: peek definition of function/class under cursor
-        -- TODO: check if this is useful
-        -- lsp_interop = {
-        --   enable = true,
-        --   border = "single",
-        --   peek_definition_code = {
-        --     ["<leader>df"] = "@function.outer",
-        --     ["<leader>dF"] = "@class.outer",
-        --   },
-        -- },
-      },
     },
+    config = function(_, opts)
+      local ts = require("nvim-treesitter")
+
+      ---@diagnostic disable-next-line: redundant-parameter
+      ts.setup({
+        install_dir = opts.install_dir,
+      })
+
+      -- Install parsers you collected from all language modules
+      local wanted = uniq(opts.ensure_installed)
+      if #wanted > 0 then
+        local handle = ts.install(wanted)
+        if opts.sync_install and handle and handle.wait then
+          pcall(function()
+            handle:wait(300000)
+          end) -- 5 min max
+        end
+      end
+
+      -- Enable features (main rewrite expects you to do this yourself) :contentReference[oaicite:4]{index=4}
+      local group = vim.api.nvim_create_augroup("UserTreesitterMain", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        callback = function(ev)
+          if opts.highlight and opts.highlight.enable then
+            pcall(vim.treesitter.start, ev.buf)
+          end
+
+          if opts.fold and opts.fold.enable then
+            vim.wo.foldmethod = "expr"
+            vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+          end
+
+          if opts.indent and opts.indent.enable then
+            vim.bo[ev.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+          end
+        end,
+      })
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    lazy = false,
+
+    opts = {
+      select = { lookahead = true },
+      move = { set_jumps = true },
+    },
+
+    config = function(_, opts)
+      require("nvim-treesitter-textobjects").setup(opts)
+
+      local select = require("nvim-treesitter-textobjects.select")
+      local move = require("nvim-treesitter-textobjects.move")
+
+      local function sel(lhs, query)
+        vim.keymap.set({ "x", "o" }, lhs, function()
+          select.select_textobject(query, "textobjects")
+        end, { desc = "TS select " .. query })
+      end
+
+      -- your existing selection maps
+      sel("af", "@function.outer")
+      sel("if", "@function.inner")
+      sel("ac", "@class.outer")
+      sel("ic", "@class.inner")
+      sel("ap", "@parameter.outer")
+      sel("ip", "@parameter.inner")
+
+      local function nxo(lhs, fn, query)
+        vim.keymap.set({ "n", "x", "o" }, lhs, function()
+          fn(query, "textobjects")
+        end, { desc = "TS move " .. query })
+      end
+
+      -- your existing movement maps
+      nxo("]f", move.goto_next_start, "@function.outer")
+      nxo("]c", move.goto_next_start, "@class.outer")
+      nxo("]a", move.goto_next_start, "@parameter.inner")
+
+      nxo("]F", move.goto_next_end, "@function.outer")
+      nxo("]C", move.goto_next_end, "@class.outer")
+      nxo("]A", move.goto_next_end, "@parameter.inner")
+
+      nxo("[f", move.goto_previous_start, "@function.outer")
+      nxo("[c", move.goto_previous_start, "@class.outer")
+      nxo("[a", move.goto_previous_start, "@parameter.inner")
+
+      nxo("[F", move.goto_previous_end, "@function.outer")
+      nxo("[C", move.goto_previous_end, "@class.outer")
+      nxo("[A", move.goto_previous_end, "@parameter.inner")
+    end,
   },
 }
